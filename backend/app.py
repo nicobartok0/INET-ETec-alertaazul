0e7156023f83aa1cb80bed58575c7ce09e0c9a7b
nicobartok0 <nicobartok0@gmail.com>
2022-11-15 08:32:31 -0300

    Flask inicial

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
-print("Hola")
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'gbbs_db'
+mysql = MySQL(app)
+
+if (__name__) == '__main__':
+    app.run(port=5000, debug=True)
