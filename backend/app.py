afb088ae4a66e9cfc24ce5d32791ed004f0a20a1
nicobartok0 <nicobartok0@gmail.com>
2022-11-15 08:33:41 -0300

    Flask inicial

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'gbbs_db'
+app.config['MYSQL_DB'] = 'azul_db'
 mysql = MySQL(app)
 
 if (__name__) == '__main__':
