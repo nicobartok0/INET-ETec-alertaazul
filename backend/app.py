479f8790996b565422deaf8a08c7efaa0d2f7bcb
nicobartok0 <nicobartok0@gmail.com>
2022-11-15 08:49:11 -0300

    Segundo commit

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
+# --  IMPORTES --
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 
+# -- DEFINICIÓN DE APP COMO OBJETO DE FLASK --
 app = Flask(__name__)
 
+# -- CONFIGURACIÓN DE MYSQL --
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'azul_db'
 mysql = MySQL(app)
 
+# -- CREAR ÁREA --
+@app.route('/crear_area')
+def crear_area():
+    nombre = request.form['nombre_area']
+    
+
+
+# -- BUCLE PRINCIPAL DE LA APP --
 if (__name__) == '__main__':
     app.run(port=5000, debug=True)
